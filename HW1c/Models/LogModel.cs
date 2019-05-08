efdaba6f089b54d9e8a74dd8a75060ad525a64e9
Mike Koenig <mikekoenig@live.com>
2019-05-08 14:02:28 -0700

    Added LogModel

---
+++ HW1c/Models/LogModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HW1c.Models
+{
+    public class LogModel
+    {
+        // The Event Type
+        // Todo, change to an Enum
+        public int EventType { get; set; }
+
+        // The Value Sent by the Phone, as a string
+        public string Value { get; set; }
+
+        // Recorded Date Time at the Server
+        public DateTime RecordedDateTime { get; set; } = new DateTime();
+
+        // ID of the Phone as a quid
+        public string PhoneID { get; set; }
+
+        // App Version string
+        public string AppVersion { get; set; }
+
+        /// <summary>
+        /// Constructor for Log Model
+        /// Calls to Initialize to set initial settings
+        /// </summary>
+        public LogModel()
+        {
+            Initialize();
+        }
+
+        /// <summary>
+        /// Intialize
+        /// Sets default values, such as DateTime as needed by the system
+        /// </summary>
+        public void Initialize()
+        {
+            // None right now...
+        }
+    }
+}
