6a86df46af35e79194bb1064ab0801e98e3518c6
Mike Koenig <mikekoenig@live.com>
2019-05-08 14:11:39 -0700

    Added LogBackend

---
+++ HW1c/Backend/LogBackend.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using HW1c.Models;
+namespace HW1c.Backend
+{
+    public class LogBackend
+    {
+
+        /// <summary>
+        ///  Returns the List of Logs
+        /// </summary>
+        /// <returns></returns>
+        public List<LogViewModel> Index()
+        {
+            var myData = new List<LogViewModel>();
+
+            // TODO:  Populate some Log Data here...
+
+            return myData;
+        }
+
+    }
+}
