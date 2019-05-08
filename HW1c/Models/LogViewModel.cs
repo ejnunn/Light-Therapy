a71fd303972edeae7436a51f1581a3e0119a2ad6
Mike Koenig <mikekoenig@live.com>
2019-05-08 14:07:39 -0700

    Added LogViewModel

---
+++ HW1c/Models/LogViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HW1c.Models
+{
+    public class LogViewModel
+    {
+        /// <summary>
+        /// List of Logs
+        /// </summary>
+        public List<LogModel> LogList { get; set; }
+    }
+}
