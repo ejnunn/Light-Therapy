e2ae4279c8de1a6e4ad436e3acf4498fc4ff5de4
Parabhpreet Dhaliwal <dhaliwalpara@seattleu.edu>
2019-06-04 12:54:52 -0700

    Added weeklyreport controller.

--- HW1c/Controllers/ReportController.cs
+++ HW1c/Controllers/ReportController.cs
@@ -25,5 +25,10 @@ namespace HW1c.Controllers
         {
             return View();
         }
+
+        public ActionResult WeeklyReport()
+        {
+            return View();
+        }
     }
 }
